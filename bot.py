d382d3ce8743561c4b333f61e92f4d7c00094f9d
BuildTools <unconfigured@null.spigotmc.org>
2020-03-05 16:26:51 -0600

    creation of config file
    in order to hide token!

--- bot.py
+++ bot.py
@@ -9,14 +9,11 @@ from discord.ext import commands
 messages = joined = 0
 bot = commands.Bot(command_prefix="meow.")
 
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines [0].strip()
+with open('config.json', 'r') as fp:
+    token = json.load(fp)['token']
 
 client = discord.Client()  # stores the client into variable
 
-token = read_token()
 
 @client.event
 async def on_ready():
