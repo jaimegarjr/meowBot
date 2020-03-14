2a94a242c8457c3cd7f7bac418f807db52586d91
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-03-13 23:45:15 -0500

    creation of cogs folder

---
+++ Cogs/misc.py
@@ -0,0 +1,55 @@
+# general imported pip modules
+import random
+import os
+
+# imported discord modules
+import discord
+from discord.ext import commands
+from discord.utils import get
+from dotenv import load_dotenv
+load_dotenv()
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def quote(self, ctx):
+        await ctx.channel.send(random.choice(list(open('Files/quotes.txt'))))
+
+
+    @commands.command()
+    async def dadprogjoke(self, ctx):
+        quote = random.choice(list(open('Files/jokes.txt')))
+        quoteQ = quote[1:quote.find("A")]
+        quoteA = quote[quote.find("A") - 1:-2]
+
+        embed = discord.Embed(title="**Dad-Styled Programming Joke**",
+                            description="**Chosen at random!**",
+                            color=discord.Colour.light_grey())
+        embed.add_field(name="```Question```", value=quoteQ, inline=False)
+        embed.add_field(name="```Answer```", value=quoteA, inline=False)
+        await ctx.channel.send(content=None, embed=embed)
+
+
+    @commands.command()
+    async def jojo(self, ctx):
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice:
+            voice.play(discord.FFmpegPCMAudio("Songs/giorno.mp3"))
+            voice.source = discord.PCMVolumeTransformer(voice.source)
+            voice.source.volume = 0.10
+            await ctx.send("JOJO REFERENCE!")
+            print("JOJO Playing!")
+
+        elif voice.is_playing():
+            await ctx.send("Bot already playing! Use m.stop to stop the current song and play a new song. -.-")
+            return
+
+        else:
+            await ctx.send("Join a voice channel first! -.-")
+            return
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
--- bot.py
+++ bot.py
@@ -10,13 +10,6 @@ from discord.ext import commands
 from discord.utils import get
 import youtube_dl
 from dotenv import load_dotenv
-import discord.opus
-
-# loading in classes from other files
-from intro import Intro
-from music import Music
-from general import General
-from misc import Misc
 
 # initial commands before running bot
 load_dotenv()
@@ -27,17 +20,16 @@ bot.remove_command("help")
 bot_id = bot.get_guild(os.environ.get("CLIENT_ID"))  # the server is found with the client id
 token = os.environ.get("BOT_TOKEN")
 
-# loaded cogs
-bot.add_cog(Intro(bot))
-bot.add_cog(Music(bot))
-bot.add_cog(General(bot))
-bot.add_cog(Misc(bot))
-
 @bot.event
 async def on_ready():
     status = discord.Game("meow :3 | m.help")
     await bot.change_presence(status=discord.Status.online, activity=status)
     print("Bot Up and Running!")
 
+extensions = ['Cogs.intro', 'Cogs.general', 'Cogs.music', 'Cogs.misc']
+
+if __name__ == '__main__':
+    for ext in extensions:
+        bot.load_extension(ext)
 
 bot.run(token)  # where the bot will run (discord server)
