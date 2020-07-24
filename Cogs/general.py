4cc7ec680ce41765852b2c7eafcc37a9665406ad
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-07-23 22:09:09 -0500

    added logo.jpg for thumbnail use

--- Cogs/general.py
+++ Cogs/general.py
@@ -22,14 +22,17 @@ class General(commands.Cog):
         embed = discord.Embed(title="**meowBot >.< General Commands**",
                               description="**Some useful commands to access meowBot:**",
                               color=discord.Colour.red())
-        embed.set_thumbnail(
-            url="https://mlcwpg.dm.files.1drv.com/y4mstMPZZ0BF8a2KPxrd"
-                "-3pFxsmTLzWHMHKhjB8D9vR88fQ8WHwQ6hpa2UcoXEoD7t2UGD5l4UDYKbkxSmnENKBs_p1BCT8Va_ugYBFClE2dlT7yDwKBZ70Cijmdfn_fXqg3xtLWRBfAK_Wz005h7bGq6BLRmhnBNevS0pp2AZ7A6Gm_DsV02U0ToSnzPnDGpXVcTgdG0nAlQ5lREViqrHAbQ?width=500&height=500&cropmode=none")
-        embed.add_field(name="```m.help```", value="Lists the commands currently available for the user.", inline=False)
-        embed.add_field(name="```m.musichelp```", value="Lists the commands to access music functions.", inline=False)
-        embed.add_field(name="```m.misc```", value="Lists fun and miscellaneous functions.", inline=False)
-        embed.add_field(name="```m.intro```", value="Greets the user.", inline=False)
-        embed.add_field(name="```m.users```", value="Prints number of users.", inline=False)
+        # FIXME: embed.set_thumbnail(url="LINKTOGITHUBLOGOHERE")
+        embed.add_field(
+            name="```m.help```", value="Lists the commands currently available for the user.", inline=False)
+        embed.add_field(name="```m.musichelp```",
+                        value="Lists the commands to access music functions.", inline=False)
+        embed.add_field(
+            name="```m.misc```", value="Lists fun and miscellaneous functions.", inline=False)
+        embed.add_field(name="```m.intro```",
+                        value="Greets the user.", inline=False)
+        embed.add_field(name="```m.users```",
+                        value="Prints number of users.", inline=False)
         embed.add_field(name="```m.purge (num)```",
                         value="Purges however many messages you provide it prior to sending command.",
                         inline=False)
@@ -41,9 +44,7 @@ class General(commands.Cog):
         embed = discord.Embed(title="**meowBot >.< Music Commands**",
                               description="**Some useful commands to access meowBot's music functionality:**",
                               color=discord.Colour.red())
-        embed.set_thumbnail(
-            url="https://mlcwpg.dm.files.1drv.com/y4mstMPZZ0BF8a2KPxrd"
-                "-3pFxsmTLzWHMHKhjB8D9vR88fQ8WHwQ6hpa2UcoXEoD7t2UGD5l4UDYKbkxSmnENKBs_p1BCT8Va_ugYBFClE2dlT7yDwKBZ70Cijmdfn_fXqg3xtLWRBfAK_Wz005h7bGq6BLRmhnBNevS0pp2AZ7A6Gm_DsV02U0ToSnzPnDGpXVcTgdG0nAlQ5lREViqrHAbQ?width=500&height=500&cropmode=none")
+        # FIXME: embed.set_thumbnail(url="LINKTOGITHUBLOGOHERE")
         embed.add_field(name="```m.join```",
                         value="Adds the bot to a voice channel if user is already in one. Otherwise, nothing will "
                               "happen.",
@@ -71,9 +72,7 @@ class General(commands.Cog):
         embed = discord.Embed(title="**meowBot >.< Misc Commands**",
                               description="**Some fun and miscellaneous functions that meowBot offers:**",
                               color=discord.Colour.red())
-        embed.set_thumbnail(
-            url="https://mlcwpg.dm.files.1drv.com/y4mstMPZZ0BF8a2KPxrd"
-                "-3pFxsmTLzWHMHKhjB8D9vR88fQ8WHwQ6hpa2UcoXEoD7t2UGD5l4UDYKbkxSmnENKBs_p1BCT8Va_ugYBFClE2dlT7yDwKBZ70Cijmdfn_fXqg3xtLWRBfAK_Wz005h7bGq6BLRmhnBNevS0pp2AZ7A6Gm_DsV02U0ToSnzPnDGpXVcTgdG0nAlQ5lREViqrHAbQ?width=500&height=500&cropmode=none")
+        # FIXME: embed.set_thumbnail(url="LINKTOGITHUBLOGOHERE")
         embed.add_field(name="```m.quote```", value="Prints a random quote for you fellas feeling under the weather.",
                         inline=False)
         embed.add_field(name="```m.github```",
@@ -95,7 +94,8 @@ class General(commands.Cog):
     # command to display amount of users on server
     @commands.command()
     async def users(self, ctx):
-        bot_id = self.bot.get_guild(int(os.environ.get("CLIENT_ID")))  # the server is found with the client id
+        # the server is found with the client id
+        bot_id = self.bot.get_guild(int(os.environ.get("CLIENT_ID")))
         users_embed = discord.Embed(title="**User Count!**",
                                     description=f"""Number of Members: {bot_id.member_count}""",
                                     colour=discord.Colour.green())
