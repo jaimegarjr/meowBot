4ed3471812a47405c7a5e32ddb15a0caf97eb5f1
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-08-04 00:12:13 -0500

    optimized creation.py

--- Cogs/creation.py
+++ Cogs/creation.py
@@ -7,7 +7,7 @@ from discord.utils import get
 # manage_channels must be true in order to allow making and managing channels
 def can_create():
     async def predicate(ctx):
-        member = ctx.message.author
+        member = ctx.author
         return member.guild_permissions.manage_channels
     return commands.check(predicate)
 
@@ -21,78 +21,67 @@ class Creation(commands.Cog):
     @commands.group(invoke_without_command=True)
     @can_create()
     async def create(self, ctx, *, new_channel):
-        member = ctx.message.author # stores member in a variable
-        guild = ctx.guild # stores the guild into a variable
 
-        channel = await guild.create_text_channel(new_channel, overwrites=None, category=guild.categories[1], reason=None)
+        channel = await ctx.guild.create_text_channel(new_channel, category=ctx.guild.categories[1])
         await channel.send(f"Text channel {new_channel} was created!")
 
     # command to create a basic voice channel
     @create.command()
     async def voice(self, ctx, *, new_channel):
-        member = ctx.message.author
-        guild = ctx.guild
 
         # note: ctx.guild.categories will return a list of possible categories that the server has
         # you can then select where the channel will be created in the category list
-        channel = await guild.create_voice_channel(new_channel, overwrites=None, category=guild.categories[2], reason=None)
+        channel = await ctx.guild.create_voice_channel(new_channel, category=ctx.guild.categories[2])
         await ctx.send(f"Voice channel {new_channel} was created!")
 
     # command to create a private text channel
     @create.command()
     async def priv(self, ctx, *, new_channel):
-        member = ctx.message.author
-        guild = ctx.guild
         
         # use discord.utils.get() for retrieving and storing a role into variables
         # guild.roles is an iterable, and name is an attribute to search for
-        admin = get(guild.roles, name="Supreme Piano Ruler")
-        mods = get(guild.roles, name="Black Keys")
+        admin = get(ctx.guild.roles, name="Supreme Piano Ruler")
+        mods = get(ctx.guild.roles, name="Black Keys")
 
         # using a dictionary, permissions can be chosen for the new channel
         # guild.default_role is @everyone, guild.me is the bot itself
         # using admin and mods allows to include them into the new channel
         overwrites = {
-            guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            guild.me: discord.PermissionOverwrite(read_messages=True),
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
             admin: discord.PermissionOverwrite(read_messages=True),
             mods: discord.PermissionOverwrite(read_messages=True)
         }
 
-        channel = await guild.create_text_channel(new_channel, overwrites=overwrites, category=guild.categories[3], reason=None)
+        channel = await ctx.guild.create_text_channel(new_channel, overwrites=overwrites, category=ctx.guild.categories[3])
         await channel.send(f"Private text channel {new_channel} was created!")
 
     # command to create a private voice channel
     @create.command()
     async def priv_voice(self, ctx, *, new_channel):
-        member = ctx.message.author
-        guild = ctx.guild
-        
-        admin = get(guild.roles, name="Supreme Piano Ruler")
-        mods = get(guild.roles, name="Black Keys")
+
+        admin = get(ctx.guild.roles, name="Supreme Piano Ruler")
+        mods = get(ctx.guild.roles, name="Black Keys")
 
         overwrites = {
-            guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            guild.me: discord.PermissionOverwrite(read_messages=True),
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
             admin: discord.PermissionOverwrite(read_messages=True),
             mods: discord.PermissionOverwrite(read_messages=True)
         }
 
-        channel = await guild.create_voice_channel(new_channel, overwrites=overwrites, category=guild.categories[2], reason=None)
+        channel = await ctx.guild.create_voice_channel(new_channel, overwrites=overwrites, category=ctx.guild.categories[2])
         await ctx.send(f"Private voice channel {new_channel} was created!")
 
     # command to delete a given channel
     @commands.command()
     @can_create()
     async def delete(self, ctx, *, channel_name):
-        member = ctx.message.author
-        guild = ctx.guild
 
         # using discord.utils.get() and bot.get_all_channels(), you can specify an attribute
         # to search through an iterable, in this case all the channels on a guild
         channel = get(self.bot.get_all_channels(), name=channel_name)
         await channel.delete()
-
         await ctx.send(f"Channel {channel_name} was deleted!")
 
 # setup method to add bot
