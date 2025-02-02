cf4a3db0e51f49c889dbeea08dc72d3d0b43174e
jaimegarjr <jjgarcia2725@gmail.com>
2025-02-02 10:24:50 -0600

    add pr checks pipeline

--- meowbot/application/services/command_handler_service.py
+++ meowbot/application/services/command_handler_service.py
@@ -1,11 +1,11 @@
-from discord.ext import commands
 from meowbot.utils import logging, setup_logger
+
 
 class CommandHandlerService:
     def __init__(self):
         self.logger = setup_logger(name="service.command.handler", level=logging.INFO)
         self.commands_tally = {}
-    
+
     # TODO: can be improved by metric firing on command usage
     def serve_on_command_event(self, ctx):
         if ctx.command is not None:
@@ -14,4 +14,3 @@ class CommandHandlerService:
             else:
                 self.commands_tally[ctx.command.name] = 1
         self.logger.info(f"{ctx.command.name} was served.")
-    
